4d0fa00c21a0b420105532959dc119eda2ef85cc
zenith-noble01 <webdev686@gmail.com>
2023-05-10 19:01:03 +0100

    added react-intersection-observer to make repeat the annimation on the hero.jsx section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,29 +1,44 @@
-import { motion } from "framer-motion";
-import { buttonVariant, heroVariant } from "../utils/motion";
+import { motion, useAnimation } from "framer-motion";
+import "../styles/hero.scss";
+import { Link } from "react-router-dom";
+import { fadeIn } from "../utils/motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 const Hero = () => {
+  const { ref, inView } = useInView();
+
+  const control = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("show");
+    } else {
+      control.start("hidden");
+    }
+  }, [inView, control]);
+
   return (
-    <motion.section
-      className="hero"
-      id="home"
-      variants={heroVariant}
+    <motion.div
+      variants={fadeIn("up", "tween", 0.3, 0.5)}
       initial="hidden"
-      animate="show"
+      animate={control}
+      className="landing__hero"
+      id="home"
+      ref={ref}
     >
-      <h1>Create Stunning Animations with Framer Motion</h1>
+      <h1>
+        Create Stunning Animations <br /> with Framer Motion
+      </h1>
       <p>
-        MotionCanvas is an online playground for Framer Motion that lets you
+        MotionCanvas is an online playground for Framer Motion that lets you{" "}
+        <br />
         visually create and customize animations and transitions with ease.
       </p>
-      <motion.button
-        variants={buttonVariant}
-        whileHover="hover"
-        className="cta-btn"
-        href="#"
-      >
-        Get Started
+      <motion.button whileHover="hover" className="cta-btn" href="a">
+        <Link to="/signup">Get Started for Free</Link>
       </motion.button>
-    </motion.section>
+    </motion.div>
   );
 };
 
