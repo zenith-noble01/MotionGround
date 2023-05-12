79a1bf411c66435b261855db363b54fe7ec5ee11
zenith-noble01 <webdev686@gmail.com>
2023-05-12 22:12:35 +0100

    started on the register page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/auth.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <div className="app__auth login">Login</div>;
 };
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/auth.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const { username, email, password } = data;
+
+  const handleOnChange = (e) => {
+    setData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {};
+
+  return (
+    <div className="app__auth login">
+      <div className="auth__container">
+        <form onSubmit={handleSubmit}></form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
