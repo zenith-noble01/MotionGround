7c09a41c846dd30c2d478615b2869b0a2fc8283e
zenith-noble01 <webdev686@gmail.com>
2023-05-17 10:56:38 +0100

    added the font-family to the buttons and all inputs

--- client/src/components/ControlPanel.jsx
+++ client/src/components/ControlPanel.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "../styles/controlPanel.scss";
 
 const ControlPanel = () => {
-  return <div className="play__control">ControlPanel</div>;
+  return (
+    <div className="play__control">
+      <div className="control__container">
+        <div className="sm__control">1</div>
+        <div className="hg__control">3</div>
+      </div>
+    </div>
+  );
 };
 
 export default ControlPanel;
