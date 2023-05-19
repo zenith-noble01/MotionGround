d892dbe114ae1504137e241f6894f20ed56a5b08
zenith-noble01 <webdev686@gmail.com>
2023-05-19 21:06:30 +0100

    made some changes

---
+++ client/src/assets/index.js
@@ -0,0 +1,4 @@
+import user from "./user.jpg"
+
+
+export { user }
--- client/src/components/Mini.jsx
+++ client/src/components/Mini.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "../styles/mini.scss";
+import { user } from "../assets";
 
 const Mini = () => {
-  return <div className="mini">Mini</div>;
+  return (
+    <div className="mini">
+      <div className="mini__container">
+        <div className="mini__routes"></div>
+        <div className="mini__profile">
+          <img src={user} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mini;
