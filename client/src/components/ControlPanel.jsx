2322b51d0c7b25cc1c24187a30ca931248622d3a
zenith-noble01 <webdev686@gmail.com>
2023-05-19 14:04:21 +0100

    designing the base elements

--- client/src/components/ControlPanel.jsx
+++ client/src/components/ControlPanel.jsx
@@ -3,14 +3,30 @@ import "../styles/controlPanel.scss";
 
 const ControlPanel = () => {
   const [active, setActive] = useState(false);
+  console.log(active);
+
   return (
     <div className="play__control">
       <div className="control__container">
         <div className="sm__control">1</div>
         <div className="hg__control">
           <div className="control__header">
-            <button>Create</button>
-            <button>Edit</button>
+            <button
+              className={!active ? "header btn active" : "headerBtn"}
+              onClick={() => setActive((prev) => !prev)}
+            >
+              Create
+            </button>
+            <button
+              className={active ? "header btn active" : "headerBtn"}
+              onClick={() => setActive((prev) => !prev)}
+            >
+              Edit
+            </button>
+          </div>
+          <div className="elements__container">
+            <div className="base__elements"></div>
+            <div className="Layouts__elements"></div>
           </div>
         </div>
       </div>
