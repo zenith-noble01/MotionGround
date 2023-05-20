b6a5a2797e77d35b9848fd20a33e68aa61bf3946
zenith-noble01 <webdev686@gmail.com>
2023-05-20 14:39:32 +0100

    made some modern css in the logo

--- client/src/components/Mini.jsx
+++ client/src/components/Mini.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/mini.scss";
+import { Link } from "react-router-dom";
 import { user } from "../assets";
 
 const Mini = () => {
@@ -7,10 +8,12 @@ const Mini = () => {
     <div className="mini">
       <div className="mini__container">
         <div className="mini__routes">
-          <div className="logo">
-            <div className="abstrac1"></div>
-            <div className="abstrac2"></div>
-          </div>
+          <Link to="/dashboard">
+            <div className="logo">
+              <div className="abstrac1"></div>
+              <div className="abstrac2"></div>
+            </div>
+          </Link>
         </div>
         <div className="mini__profile">
           <img src={user} alt="" />
