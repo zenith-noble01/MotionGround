79d9f3648d792e19b6ea844bb200035f392cdc02
zenith-noble01 <webdev686@gmail.com>
2023-05-21 15:34:32 +0100

    on the AnnimateController

--- client/src/components/AnimateContainer.jsx
+++ client/src/components/AnimateContainer.jsx
@@ -1,5 +1,42 @@
 const AnimateContainer = () => {
-  return <div className="annimate__container">AnimateContainer</div>;
+  return (
+    <div className="annimate__container">
+      <div className="container__wrapper">
+        <div className="animation__prop">
+          <div className="annimationHeader">
+            <p>Direction</p>
+          </div>
+          <div className="animation__content">
+            <select name="annimation type"></select>
+          </div>
+        </div>
+        <div className="animation__prop">
+          <div className="annimationHeader">
+            <p>Animation type</p>
+          </div>
+          <div className="animation__content">
+            <select name="annimation type"></select>
+          </div>
+        </div>
+        <div className="animation__prop">
+          <div className="annimationHeader">
+            <p>Delay</p>
+          </div>
+          <div className="animation__content">
+            <input type="number" name="" id="" />
+          </div>
+        </div>
+        <div className="animation__prop">
+          <div className="annimationHeader">
+            <p>Duration</p>
+          </div>
+          <div className="animation__content">
+            <input type="number" name="" id="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AnimateContainer;
