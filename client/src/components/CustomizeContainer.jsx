58dd4ca5b8093e833a2ba800f15d394277f36a79
zenith-noble01 <webdev686@gmail.com>
2023-05-25 04:00:39 +0100

    started working on the customizing page

--- client/src/components/CustomizeContainer.jsx
+++ client/src/components/CustomizeContainer.jsx
@@ -1,5 +1,25 @@
+import { RxMargin, RxPadding } from "react-icons/rx";
+import "../styles/custimize.scss";
+
 const CustomizeContainer = () => {
-  return <div className="customize__container">CustomizeContainer</div>;
+  return (
+    <div className="customize__container">
+      <div className="customize__header">
+        <p>Customize your Elements</p>
+      </div>
+      <div className="customize__content">
+        <div className="customize">
+          <div className="header">Spacing</div>
+        </div>
+        <div className="customize">
+          <div className="header">Dimensioins</div>
+        </div>
+        <div className="customize">
+          <div className="header">Background</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CustomizeContainer;
