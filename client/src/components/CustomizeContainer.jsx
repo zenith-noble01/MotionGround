df34904224eef79c2ed38568556a64493efa9445
zenith-noble01 <webdev686@gmail.com>
2023-05-25 04:26:33 +0100

    some changes on the customize

--- client/src/components/CustomizeContainer.jsx
+++ client/src/components/CustomizeContainer.jsx
@@ -10,10 +10,35 @@ const CustomizeContainer = () => {
       <div className="customize__content">
         <div className="customize">
           <div className="header">Spacing</div>
+          <div className="customizer">
+            <p>
+              <RxPadding /> Padding
+            </p>
+            <div className="space__content">
+              <input type="number" placeholder="u" />
+              <input type="number" placeholder="r" />
+              <input type="number" placeholder="b" />
+              <input type="number" placeholder="l" />
+            </div>
+          </div>
+          <div className="customizer">
+            <p>
+              <RxMargin /> Margin
+            </p>
+            <div className="space__content">
+              <input type="number" placeholder="u" />
+              <input type="number" placeholder="r" />
+              <input type="number" placeholder="b" />
+              <input type="number" placeholder="l" />
+            </div>
+          </div>
         </div>
-        <div className="customize">
+        {/* <div className="customize">
           <div className="header">Dimensioins</div>
-        </div>
+          <div className="customizer">
+            <div className="x"></div>
+          </div>
+        </div> */}
         <div className="customize">
           <div className="header">Background</div>
         </div>
