97b119d444ef45fba702d09725ae59cbbeec7131
zenith-noble01 <webdev686@gmail.com>
2023-06-03 16:45:45 +0100

    don't even know what I'm sending

--- client/src/components/actionPanel/Customization/CustomizeContainer.jsx
+++ client/src/components/actionPanel/Customization/CustomizeContainer.jsx
@@ -82,6 +82,7 @@ const CustomizeContainer = () => {
             }}
           >
             <Padding />
+            <div className="line" />
             <Margin />
           </motion.div>
         </div>
